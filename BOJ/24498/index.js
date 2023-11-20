0735c71fc1d9f4456b2814e05f2fbae11c5f824c
turtle601 <turtle601@naver.com>
2023-11-20 11:01:39 +0900

    feat: 24498 blobnom 문제 해결

--- BOJ/24498/index.js
+++ BOJ/24498/index.js
@@ -1,5 +1,26 @@
-// const input = require('fs').readFileSync(0, 'utf-8').trim();
+const input = require('fs').readFileSync(0, 'utf-8').trim();
 
-function solution() {}
+function solution(input) {
+  const [[towerCount], towerArr] = input.split('\n').map((item) =>
+    item
+      .trim()
+      .split(' ')
+      .map((value) => Number(value))
+  );
 
-module.exports = solution;
+  if (towerCount <= 2) {
+    return Math.max(...towerArr);
+  } else {
+    const res = Array(towerCount - 2)
+      .fill(0)
+      .map((_, idx) => {
+        return towerArr[idx + 1] + Math.min(towerArr[idx], towerArr[idx + 2]);
+      });
+
+    return Math.max(...res, towerArr[0], towerArr[towerCount - 1]);
+  }
+}
+
+console.log(solution(input));
+
+// module.exports = solution;
--- BOJ/24498/index.test.js
+++ BOJ/24498/index.test.js
@@ -1,7 +1,7 @@
-const { solution } = require('.');
+const solution = require('.');
 
 describe('blobnom 문제', () => {
-  test('예제 입력 1', () => {
+  it('예제 입력 1', () => {
     const input = `4
     1 3 2 2`;
 
@@ -10,11 +10,35 @@ describe('blobnom 문제', () => {
     expect(solution(input)).toEqual(answer);
   });
 
-  test('예제 입력 2', () => {
+  it('예제 입력 2', () => {
     const input = `5
     2 4 3 5 5`;
 
     const answer = 8;
+    expect(solution(input)).toEqual(answer);
+  });
+
+  it('반례 1: tower의 개수가 1일 경우', () => {
+    const input = `1
+    2`;
+
+    const answer = 2;
+    expect(solution(input)).toEqual(answer);
+  });
+
+  it('반례 2: tower의 개수가 2일 경우', () => {
+    const input = `2
+    2 4`;
+
+    const answer = 4;
+    expect(solution(input)).toEqual(answer);
+  });
+
+  it('반례 3: 타워의 수가 양 끝이 가장 높을 때', () => {
+    const input = `2
+    2 1 6 12`;
+
+    const answer = 12;
     expect(solution(input)).toEqual(answer);
   });
 });
